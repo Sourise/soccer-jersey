62287b5f224cc2d512934ad43c6032d7cd8579c3
unknown <souleimanb@outlook.fr>
2021-09-06 11:40:28 +0200

    Formulaire et connexion

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+
+    private $passwordHasher;
+
+    public function __construct( EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher) {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function index(Request $request, UserPasswordHasherInterface $encoder)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+            $user->setPassword($this->passwordHasher->hashPassword(
+                $user,
+                $form->get('password')->getData()
+            ));
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
