71c4d4b451fdf229063f079d0003c6e7b2a5eb85
Souleiman Roullin <souleiman.r@mairiepia.local>
2021-10-07 13:06:27 +0200

    widget

---
+++ src/Controller/ResultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ResultController extends AbstractController
+{
+    /**
+     * @Route("/resultats-ligue1", name="results")
+     */
+    public function index(): Response
+    {
+        return $this->render('result/index.html.twig');
+    }
+}
